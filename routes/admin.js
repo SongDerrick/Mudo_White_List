fa989f88bbeaa2ac4b92bb8af4c9c10bf653b9e9
sd990307@gmail.com <songdoyoon@songdoyun-ui-MacBook-Pro.local>
2022-07-06 10:49:22 +0900

    Views update

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/add-products",(req, res, next)=>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/add-products', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
