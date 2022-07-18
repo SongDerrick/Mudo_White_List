9340a61b2057a20d7903c93283056bc85e6dba97
SongDerrick <sd990307@gmail.com>
2022-07-18 16:46:53 +0900

    few changes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 // });
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const path = require("path");
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
-router.get("/add-products",(req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+router.get("/add-product",(req, res, next)=>{
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/add-products', (req, res, next)=>{
+router.post('/add-product', (req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
 });
