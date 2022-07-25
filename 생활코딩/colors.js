054b57cc907d4c0896900139b8181fe3febe2044
myamyam <105202019+myamyam@users.noreply.github.com>
2022-07-26 01:59:18 +0900

    Update colors.js

--- 생활코딩/colors.js
+++ 생활코딩/colors.js
@@ -32,3 +32,23 @@ function nightDayHandler(self){
 Links.setColor('blue');
       }
   }
+
+/*
+JavaScript! 사용자와 상호작용
+동적인 언어, 계산기능 또한 가능; html과의 차이
+<script> 태그
+글씨 출력 -> document.write();
+<input>태그 onclick, onchange, onkeydown
+ex -> <input type="text" onchange="alert('changed')">=
+
+콘솔의 이용
+조건문
+배열과 반복문
+length는 개수를 센다
+0부터 세기 시작, var(변수) 이용
+함수
+함수는 만들기 나름이다.
+function 함수이름(){} 형태
+객체
+document.write도 객체의 한 형태였다.
+*/
